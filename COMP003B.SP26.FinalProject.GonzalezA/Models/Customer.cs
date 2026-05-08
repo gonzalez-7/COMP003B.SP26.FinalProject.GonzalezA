0731166f673e5cd95001966a4ff78a2f43bbd666
gonzalez-7 <20agonzalezp@gmail.com>
2026-05-08 16:45:13 -0700

    Added a customer model to store the barber shop customer information

---
+++ COMP003B.SP26.FinalProject.GonzalezA/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Models
+{
+	public class Customer
+	{
+		public int CustomerId { get; set; }
+
+		[Required]
+		[StringLength(30)]
+		public string Name { get; set; }
+
+
+		[Required]
+		[StringLength(20)]
+		public string PhoneNumber {  get; set; }
+
+
+		[Required]
+		[StringLength(50)]
+		public string Email { get; set; }
+
+		[StringLength(150)]
+		public string Notes { get; set; }
+
+		public virtual ICollection<Appointment>? Appointments { get; set; }
+	}
+}
