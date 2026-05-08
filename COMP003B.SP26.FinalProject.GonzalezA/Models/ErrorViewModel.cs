07f9ec073b4c0cc5e74b84802e27d46d78b6c729
gonzalez-7 <20agonzalezp@gmail.com>
2026-05-08 16:10:21 -0700

    Add project files.

---
+++ COMP003B.SP26.FinalProject.GonzalezA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.SP26.FinalProject.GonzalezA.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
