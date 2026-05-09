91e72ec7406e607b526fe783baeb875dbf68ae95
gonzalez-7 <20agonzalezp@gmail.com>
2026-05-09 14:31:37 -0700

    Scaffold MVC controllers and views for barber shop models

---
+++ COMP003B.SP26.FinalProject.GonzalezA/Controllers/AppointmentsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP26.FinalProject.GonzalezA.Data;
+using COMP003B.SP26.FinalProject.GonzalezA.Models;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppointmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Appointments
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Appointments.Include(a => a.Barber).Include(a => a.BarberService).Include(a => a.Customer);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Appointments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .Include(a => a.Barber)
+                .Include(a => a.BarberService)
+                .Include(a => a.Customer)
+                .FirstOrDefaultAsync(m => m.AppointmentId == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // GET: Appointments/Create
+        public IActionResult Create()
+        {
+            ViewData["BarberId"] = new SelectList(_context.Barbers, "BarberId", "Name");
+            ViewData["BarberServiceId"] = new SelectList(_context.BarberServices, "BarberServiceId", "Description");
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email");
+            return View();
+        }
+
+        // POST: Appointments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AppointmentId,CustomerId,BarberId,BarberServiceId,AppointmentDate,AppointmentTime,AppointmentStatus,IsConfirmed")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(appointment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BarberId"] = new SelectList(_context.Barbers, "BarberId", "Name", appointment.BarberId);
+            ViewData["BarberServiceId"] = new SelectList(_context.BarberServices, "BarberServiceId", "Description", appointment.BarberServiceId);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email", appointment.CustomerId);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            ViewData["BarberId"] = new SelectList(_context.Barbers, "BarberId", "Name", appointment.BarberId);
+            ViewData["BarberServiceId"] = new SelectList(_context.BarberServices, "BarberServiceId", "Description", appointment.BarberServiceId);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email", appointment.CustomerId);
+            return View(appointment);
+        }
+
+        // POST: Appointments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AppointmentId,CustomerId,BarberId,BarberServiceId,AppointmentDate,AppointmentTime,AppointmentStatus,IsConfirmed")] Appointment appointment)
+        {
+            if (id != appointment.AppointmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appointment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppointmentExists(appointment.AppointmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BarberId"] = new SelectList(_context.Barbers, "BarberId", "Name", appointment.BarberId);
+            ViewData["BarberServiceId"] = new SelectList(_context.BarberServices, "BarberServiceId", "Description", appointment.BarberServiceId);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "Email", appointment.CustomerId);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .Include(a => a.Barber)
+                .Include(a => a.BarberService)
+                .Include(a => a.Customer)
+                .FirstOrDefaultAsync(m => m.AppointmentId == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // POST: Appointments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment != null)
+            {
+                _context.Appointments.Remove(appointment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return _context.Appointments.Any(e => e.AppointmentId == id);
+        }
+    }
+}
---
+++ COMP003B.SP26.FinalProject.GonzalezA/Controllers/BarberServicesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP26.FinalProject.GonzalezA.Data;
+using COMP003B.SP26.FinalProject.GonzalezA.Models;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Controllers
+{
+    public class BarberServicesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BarberServicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BarberServices
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BarberServices.ToListAsync());
+        }
+
+        // GET: BarberServices/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var barberService = await _context.BarberServices
+                .FirstOrDefaultAsync(m => m.BarberServiceId == id);
+            if (barberService == null)
+            {
+                return NotFound();
+            }
+
+            return View(barberService);
+        }
+
+        // GET: BarberServices/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BarberServices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BarberServiceId,ServiceName,Description,Price,EstimatedMinutes")] BarberService barberService)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(barberService);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(barberService);
+        }
+
+        // GET: BarberServices/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var barberService = await _context.BarberServices.FindAsync(id);
+            if (barberService == null)
+            {
+                return NotFound();
+            }
+            return View(barberService);
+        }
+
+        // POST: BarberServices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BarberServiceId,ServiceName,Description,Price,EstimatedMinutes")] BarberService barberService)
+        {
+            if (id != barberService.BarberServiceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(barberService);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BarberServiceExists(barberService.BarberServiceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(barberService);
+        }
+
+        // GET: BarberServices/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var barberService = await _context.BarberServices
+                .FirstOrDefaultAsync(m => m.BarberServiceId == id);
+            if (barberService == null)
+            {
+                return NotFound();
+            }
+
+            return View(barberService);
+        }
+
+        // POST: BarberServices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var barberService = await _context.BarberServices.FindAsync(id);
+            if (barberService != null)
+            {
+                _context.BarberServices.Remove(barberService);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BarberServiceExists(int id)
+        {
+            return _context.BarberServices.Any(e => e.BarberServiceId == id);
+        }
+    }
+}
