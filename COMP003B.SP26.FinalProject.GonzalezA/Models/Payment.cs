d56e8ab712430403b64b2250b18e2e499df42df5
gonzalez-7 <20agonzalezp@gmail.com>
2026-05-08 17:44:43 -0700

    added a model to track the appointments payment information

---
+++ COMP003B.SP26.FinalProject.GonzalezA/Models/Payment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Models
+{
+	public class Payment
+	{
+		public int PaymentId { get; set; }
+
+		public int AppointmentId { get; set; }
+
+
+		[Range(1, 200)]
+		public int Amount { get; set; }
+
+		[Required]
+		[StringLength(15)]
+		public string PaymentMethod { get; set; }
+
+		[Required]
+		[StringLength(25)]
+		public string PaymentStatus { get; set; }
+
+		public bool IsPaid { get; set; }
+
+		public virtual Appointment? Appointment { get; set; }
+	}
+}
