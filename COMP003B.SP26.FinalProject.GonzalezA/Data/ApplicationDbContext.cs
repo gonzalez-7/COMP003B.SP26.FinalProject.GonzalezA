504d9d7749e61c8716bf77da7b68d3dc3157eae8
gonzalez-7 <20agonzalezp@gmail.com>
2026-05-09 13:12:31 -0700

    Added ApplicationDbContext for the barber shop database models

---
+++ COMP003B.SP26.FinalProject.GonzalezA/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using COMP003B.SP26.FinalProject.GonzalezA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<Customer> Customers { get; set; }
+
+		public DbSet<Barber> Barbers { get; set; }
+
+		public DbSet<BarberService> BarbersServices { get; set; }
+
+		public DbSet<Appointment> Appointments { get; set; }
+
+		public DbSet<Payment> Payments { get; set; }
+	}
+}
