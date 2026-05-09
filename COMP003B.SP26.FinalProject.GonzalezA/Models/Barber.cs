3b53e09cff00a7011d80fab13987cf4f7e4d4234
gonzalez-7 <20agonzalezp@gmail.com>
2026-05-08 17:04:54 -0700

    Added Barber and Barber Service to store staff information and services for the barber

---
+++ COMP003B.SP26.FinalProject.GonzalezA/Models/Barber.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Models
+{
+	public class Barber
+	{
+		public int BarberId { get; set; }
+
+		[Required]
+		[StringLength(30)]
+		public string Name { get; set; }
+
+
+		[Required]
+		[StringLength(20)]
+		public string PhoneNumber { get; set; }
+
+
+		[Required]
+		[StringLength(30)]
+		public string Specialty { get; set; }
+
+		[Range(0, 40)]
+		public int YearsExperience { get; set; }
+
+		public virtual ICollection<Appointment>? Appointments { get; set; }
+	}
+}
---
+++ COMP003B.SP26.FinalProject.GonzalezA/Models/BarberService.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Models
+{
+	public class BarberService
+	{
+		public int BarberServiceId { get; set; }
+
+		[Required]
+		[StringLength (30)]
+		public string ServiceName { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Description { get; set; }
+
+
+		[Range(1, 150)]
+		public int price { get; set; }
+
+
+		[Range(10, 120)]
+		public int EstimatedMinutes { get; set; }
+
+		public virtual ICollection<Appointment>? Appointments { get; set; }
+
+	}
+}
