d75bbdb0d3ea5ce515eea8ea9112aec947d93599
gonzalez-7 <20agonzalezp@gmail.com>
2026-05-09 16:15:38 -0700

    Added request timing middleware

---
+++ COMP003B.SP26.FinalProject.GonzalezA/Middleware/RequestTimingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace COMP003B.SP26.FinalProject.GonzalezA.Middleware
+{
+	public class RequestTimingMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public RequestTimingMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			var stopwatch = Stopwatch.StartNew();
+
+			await _next(context);
+
+			stopwatch.Stop();
+
+			Console.WriteLine($"Request: {context.Request.Method} Path: {context.Request.Path} Time: {stopwatch.ElapsedMilliseconds} ms");
+		}
+	}
+}
